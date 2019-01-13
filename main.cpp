3dcf70eb01e5be5a7a5dacee659fda1113abeda2
emilraineroparc <emil.rainero@parc.com>
2019-01-12 21:26:48 -0500

    .

--- main.cpp
+++ main.cpp
@@ -2,25 +2,31 @@
 #include "Timer.h"
 
 int main() {
-//    TriangleMesh mesh("../cube_10x10x10.stl", 0.0001);
-    TriangleMesh mesh("../bblocky.stl", 0.0001);
+    double epsilon{0.0001};
+    double layerHeight{0.1};
+
+//    TriangleMesh mesh("../cube_10x10x10.stl", epsilon);
+//    TriangleMesh mesh("../bblocky.stl", epsilon);
+    TriangleMesh mesh("../pisa.stl", epsilon);
 
     PolygonSlicer slicer;
 
     Timer timer;
-    auto layers = slicer.sliceModel(mesh, 1.0);
-    std::cout << "Time: " << timer.elapsed() << std::endl;
-
+    auto layers = slicer.sliceModel(mesh, layerHeight);
+    std::cout << "Time: " << timer.elapsed() << " seconds" << std::endl;
     std::cout << "Slices: " << layers.size() << std::endl;
 
-    for (auto layer: layers) {
-        std::cout << "Layer: " << layer->z << std::endl;
+    if (false) {
+        for (auto layer: layers) {
+            std::cout << "Layer: " << layer->z << std::endl;
 
-        for (auto contour: layer->contours) {
-            std::cout << "\tContour - external: " << contour.external << " - clockwise: " << contour.clockwise << std::endl;
+            for (auto contour: layer->contours) {
+                std::cout << "\tContour - external: " << contour.external << " - clockwise: " << contour.clockwise
+                          << std::endl;
 
-            for (auto point: contour.points) {
-                std::cout << "\t\t" << point << std::endl;
+                for (auto point: contour.points) {
+                    std::cout << "\t\t" << point << std::endl;
+                }
             }
         }
     }
